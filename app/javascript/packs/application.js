b7c9cfb0830140f46fdb82773f571f131298d8a3
DandyCV <dandycvc@gmail.com>
2020-11-24 15:47:31 +0200

    Default admin user / Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,19 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
+import "bootstrap"
+import "./stylesheets/application"
+global.$ = jQuery;
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toogle="tooltip"]').tooltip()
+    $('[data-toogle="popover"]').popover()
+})
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
